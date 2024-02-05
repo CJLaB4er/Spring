065a496a791660a971e3c830c344030cc403d3aa
CJlaB4er-MSI <CJlaB4er@gmail.com>
2024-02-06 01:24:05 +0300

    Initial data added. data.sql

---
+++ Seminar2/empl4sem2CRUD/src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO userTable (firstName, lastName) VALUES ('Ivan', 'Ivanov');
+INSERT INTO userTable (firstName, lastName) VALUES ('Sergey', 'Nikolaev');
+INSERT INTO userTable (firstName, lastName) VALUES ('Artur', 'Adrianov');
+INSERT INTO userTable (firstName, lastName) VALUES ('Nikolay', 'Romanenko');
