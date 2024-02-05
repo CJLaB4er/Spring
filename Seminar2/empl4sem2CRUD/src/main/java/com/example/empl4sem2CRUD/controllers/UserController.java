15246902e1cd987eb5f34811547e020f1ab0ba29
CJlaB4er-MSI <CJlaB4er@gmail.com>
2024-02-06 01:04:03 +0300

    UserController.java added new method deleteUser

--- Seminar2/empl4sem2CRUD/src/main/java/com/example/empl4sem2CRUD/controllers/UserController.java
+++ Seminar2/empl4sem2CRUD/src/main/java/com/example/empl4sem2CRUD/controllers/UserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -16,7 +17,7 @@ public class UserController {
     private UserService userService;
 
     @GetMapping("/users")
-    public String findAll(Model model){
+    public String findAll(Model model) {
         List<User> users = userService.findAll();
 
 
@@ -26,15 +27,19 @@ public class UserController {
     }
 
     @GetMapping("/user-create")
-    public String createUserForm(User user){
+    public String createUserForm(User user) {
         return "user-create";
     }
 
     @PostMapping("/user-create")
-    public String createUser(User user){
+    public String createUser(User user) {
         userService.saveUser(user);
         return "redirect:/users";
     }
 
-    //@GetMapping("user-delete/{id}")
+    @GetMapping("user-delete/{id}")
+    String deleteUser(@PathVariable("id") int id) {
+        userService.deleteById(id);
+        return "redirect:/users";
+    }
 }
