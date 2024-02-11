5a7b2a16a0519713a8a9fc093769dad2ae36ebbb
CJlaB4er-MSI <CJlaB4er@gmail.com>
2024-02-12 02:28:17 +0300

    HW4 step2

--- Seminar4/seminar4hometask/src/main/java/ru/home/seminar4hometask/controllers/webAppController.java
+++ Seminar4/seminar4hometask/src/main/java/ru/home/seminar4hometask/controllers/webAppController.java
@@ -1,8 +1,12 @@
 package ru.home.seminar4hometask.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 @Controller
 public class webAppController {
@@ -10,4 +14,16 @@ public class webAppController {
     public String index() {
         return "index";
     }
+
+    /*Вывод текущей даты и времени через Thymeleaf*/
+    @GetMapping("/time")
+    public String time(Model model) {
+        LocalDateTime currentDatetime = LocalDateTime.now();
+        String date = currentDatetime.format(DateTimeFormatter.ofPattern("dd MMMM yyyyг.")).toString();
+        String time = currentDatetime.format(DateTimeFormatter.ofPattern("HH:mm:ss")).toString();
+        model.addAttribute("currentTime", time);
+        model.addAttribute("currentDate", date);
+        return "time";
+    }
+
 }
