64ebc6bd6efe9d46f2c910c981fd8ffdc08488c6
CJlaB4er-MSI <CJlaB4er@gmail.com>
2024-02-12 03:19:03 +0300

    HW4 step3

---
+++ Seminar4/seminar4hometask/src/main/java/ru/home/seminar4hometask/Seminar4hometaskApplication.java
@@ -0,0 +1,13 @@
+package ru.home.seminar4hometask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Seminar4hometaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Seminar4hometaskApplication.class, args);
+	}
+
+}
